371aa9fa23ab886fc128694b2099c3da71c47bad
AloneMonkey <liupeiqing1993@163.com>
2017-07-26 18:37:20 +0800

    update

--- MonkeyDevPod/MonkeyDevPod/Build/Intermediates.noindex/MonkeyDevPod.build/Release-iphonesimulator/MonkeyDevPod.build/DerivedSources/MonkeyDevPod_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char MonkeyDevPodVersionString[];
- extern const double MonkeyDevPodVersionNumber;
-
- const unsigned char MonkeyDevPodVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:MonkeyDevPod  PROJECT:MonkeyDevPod-1" "\n";
- const double MonkeyDevPodVersionNumber __attribute__ ((used)) = (double)1.;
